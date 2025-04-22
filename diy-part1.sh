2278f820d98d220ccf2df81cf51cafa52ca05ceb
hn&qi <158002374+hnonly@users.noreply.github.com>
2025-04-22 18:43:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,13 +8,13 @@
 #===============================================
 
 # 修改版本为编译日期，数字类型。
-"%Y%m%d%H"
-$date_version
+date_version=$(date +"%Y%m%d%H")
+echo $date_version > version
 
 # 为iStoreOS固件版本加上编译作者
-"hn"
--i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V ${date_version} by ${author}'/g"
--i "s/OPENWRT_RELEASE.*/OPENWRT_RELEASE=\"%D %V ${date_version} by ${author}\"/g"
+author="hn"
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V ${date_version} by ${author}'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/OPENWRT_RELEASE.*/OPENWRT_RELEASE=\"%D %V ${date_version} by ${author}\"/g" package/base-files/files/usr/lib/os-release
 
 # 拉取我的软件包仓库
-'src-git xmpackages https://github.com/xiaomeng9597/openwrt-packages2.git;main'
+echo 'src-git xmpackages https://github.com/xiaomeng9597/openwrt-packages2.git;main' >> feeds.conf.default
